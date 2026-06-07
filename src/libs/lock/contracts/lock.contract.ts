1653b0e3a08fc606be815e57c21b8adf06d796f1
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-06-07 08:16:37 +0300

    feat(infra): implement redis distributed lock module

---
+++ src/libs/lock/contracts/lock.contract.ts
@@ -0,0 +1,19 @@
+import { LockWaitOptions } from './lock-wait-options.contract';
+
+export interface ILock {
+  acquire(key: string, ttlMs: number): Promise<string | null>;
+
+  acquireWithWait(
+    key: string,
+    ttlMs: number,
+    options: LockWaitOptions,
+  ): Promise<string>;
+
+  release(key: string, token: string): Promise<boolean>;
+
+  execute<T>(
+    key: string,
+    ttlMs: number,
+    callback: () => Promise<T>,
+  ): Promise<T>;
+}
